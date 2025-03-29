62424897113fd3727592ac6b25f24ab2c18657c5
Göktuğ Yumuşak <goktug033@outlook.com>
2025-03-29 13:27:54 +0300

    changed site name & site icon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import PidMotor from './pid_motor';
 import 'react-toastify/dist/ReactToastify.css';
+import { Helmet } from 'react-helmet';
 
 
 function App() {
   return (
     <div className="App">      
       <PidMotor />
+      <Helmet>
+        <title>PID Motor Control</title>
+        <meta name="description" content="PID Motor Control Web Application" />
+        <link rel="icon" href="/favicon.ico" />
+      </Helmet>
     </div>
   );
 }
